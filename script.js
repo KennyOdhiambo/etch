80a041570c752b87b9ed15c5e40e4dde781e064a
Kennedy Odhiambo <oduorkennedy@gmail.com>
2020-12-01 05:51:36 +0300

    modified the file

--- script.js
+++ script.js
@@ -1,113 +1,111 @@
-class grid {
-    WIDTH: 980;
-    WIDTH_STR = `${this.WIDTH}px`;
+const container = document.querySelector('.container');
+const colorButtons = document.querySelectorAll('.colorChoice');
+const userColorPicker = document.querySelector('#input-color');
+const clearButton = document.querySelector('.clear');
+var slider = document.querySelector('#sizeRange');
+var color = 'black';
 
-    container = "";
-    body = document.querySelector("body");
-
-    constructor(size) {
-        this.container = document.createElement("div")
-        this.container.style.display = "grid"
-        this.container.style.width = this.WIDTH_STR
-
-        this.initializeSquares(size);
-
-        this.buttonControls = document.createElement("div")
-        this.buttonControls.style.display = "flex"
-        this.buttonControls.style.flexDirection = "row"
-
-        this.clearButton = this.makeNewButton("Clear")
-        this.clearButton.addEventListener("click", (e) => {
-            this.clearColor()
-        })
-
-        this.buttonControls.appendChild(this.clearButton)
-
-        this.sizeInput = document.createElement("input")
-        this.sizeInput.type = "number"
-        this.sizeInput.style.border = "1px solid rgba(100,100,0.5)"
-        this.buttonControls.appendChild(this.sizeInput)
-
-        //Change size of grid
-        this.resizeButton = this.makeNewButton("Resize")
-        this.resizeButton.addEventListener("click", (e) => {
-            let newSize = this.sizeInput.value
-            this.removeSquares()
-            this.initializeSquares(newSize)
-        })
-        this.buttonControls.appendChild(this.resizeButton)
-
-        //append buttons and inputs and div
-        this.body.appendChild(this.buttonControls)
-        this.body.appendChild(this.container)
+function createGrid(gridNumber) {
+    let gridArea = gridNumber * gridNumber;
+    for (let i = 1; i <= gridArea; i++) {
+        let gridItem = document.createElement('div');
+        container.style.gridTemplateColumns = `repeat($(gridNumber), 1fr)`;
+        container.style.gridTemplateRows = `repeat($(gridNumber), 1fr)`;
+        container.insertAdjacentElement('beforeend', gridItem);
     }
-    initializeSquares(size) {
-        //limit size to 100
-        if (size > 100) {
-            size = 100
-        }
-        //tell html the number of columns and rows
-        this.container.style.gridTemplateColumns = this.gridSizer(size)
-        this.container.style.gridTemplateRows = this.gridSizer(size)
+    vargridPixels = container.querySelectorAll('div');
+    gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', colorGrid));
+}
 
-        //make new square objects and append to grid 
-        let totalSquares = size * size
-        for (let i = 0; i < totalSquares; i++) {
-            const newSquare = new square()
-            this.resizeSquare(newSquare, size)
-            this.container.appendChild(newSquare.block)
-        }
-        removeSquares() {
-            while (this.container.firstChild) {
-                this.container.removeChild(this.container.lastChild)
-            }
-        }
-        resizeSquare(inputSquare, x) {
-                //devide size from needed grid width to calculate height, creating a square grid
-                let size = `${this.WIDTH/x}px`
-                inputSquare.block.style.height = size
-            }
-            //create two buttons
-        makeNewButton(string) {
-            let button = document.createElement("button")
-            button.style.border = "1px solid black"
-            button.style.padding = "1em"
-            button.textContent = string
-            return button
-        }
-        gridSizer(value) {
-            return `repeat(${value}, auto)`
-        }
-        clearColor() {
-            let allSquares = document.querySelectorAll(".square")
-            allSquares.forEach(function(item) {
-                item.style.background = "white"
-            })
-        }
-    }
-    class square {
-        block = ""
-        alpha = 0
-
-        constructor() {
-            this.block = document.createElement("div")
-
-            this.block.classList.add("square")
-            this.block.style.border = "1px solid rgb(150, 150, 150)"
-            this.block.style.background = "white"
-
-            this.block.addEventListener("mouseover", (e) => {
-                if (this.alpha < 100) {
-                    this.alpha += 10
+function colorGrid() {
+    switch (color) {
+        case 'rainbow':
+            this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`;
+            this.classList.remove('gray');
+            break;
+        case 'gray':
+            if (this.backgroundColor.match(/rgba/)) {
+                let currentOpacity = Number(this.backgroundColor.slice(-4, -1));
+                if (currentOpacity <= 0.9) {
+                    this.style.backgroundColor = `rgba(0,0,0, ${currentOpacity+0.1})`;
+                    this.classList.add('gray');
                 }
-                this.changeColor(this.alpha)
-            })
-        }
-
-        changeColor(alpha) {
-            alpha = 255 - ((255 * alpha) / 100)
-            let background = rgb($ { alpha }, $ { alpha }, $ { alpha })
-            this.block.style.background = background
-        }
+            } else if (this.classList == 'gray' && this.backgroundColor == 'rgb(0,0,0)') {
+                return;
+            } else {
+                this.style.backgroundColor = 'rgba(0,0,0, 0.1)';
+            }
+            break;
+        case 'eraser':
+            this.style.backgroundColor = '#fffff0';
+            this.classList.remove('gray');
+            break;
+        case 'black':
+            this.style.backgroundColor = '#000000';
+            this.classList.remove('gray');
+            break;
+        default:
+            this.style.backgroundColor = color;
+            this.classList.remove('gray');
+            break;
     }
-    const newGrid = new grid(64, 64)
+}
+
+//clear Button
+function eraseAllColor() {
+    var gridPixels = container.querySelectorAll('div');
+    gridPixels.forEach(gridPixel => gridPixel.style.backgroundColor = '#ffffff');
+}
+
+//to update a color variable when a color button is clicked
+function changeColor(event) {
+    switch (event.target.dataset.color) {
+        case 'rainbow':
+            color = 'rainbow';
+            break;
+        case 'gray';
+        color = 'gray';
+        break;
+
+        case 'eraser':
+            break;
+
+        default:
+            color = 'black';
+            break;
+    }
+}
+
+function pixelSize() {
+    let gridPixels = container.querySelectorAll('div');
+    gridPixels.forEach(gridPixel => gridPixel.remove());
+    createGrid(slider.velue);
+}
+
+function userColorSelection(event) {
+    color = event.target.value;
+}
+
+function buttonHover() {
+    this.style.border = '1px solid #ffffff';
+}
+
+function buttonStandard() {
+    this.style.border = '1px solid #FF0000';
+}
+
+//On Page Load - deafault size
+
+createGrid(10);
+
+//Event Listeners
+
+clearButton.addEventListener('click', eraseAllColor);
+clearButton.addEventListener('mouseover', buttonHover);
+clearButton.addEventListener('mouseout', buttonStandard);
+colorButtons.forEach(colorButton => colorButton.addEventListener('click', changeColor));
+colorButtons.forEach(colorButton => colorButton.addEventListener('mouseover', buttonHover));
+colorButtons.forEach(colorButton => colorButton.addEventListener('mouseout', buttonStandard));
+slider.addEventListener('mouseup', pixelSize);
+userColorPicker.addEventListener('change', userColorSelection, false);
+userColorPicker.addEventListener('input', userColorSelection, false);
